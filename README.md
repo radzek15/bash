ede064f725096afbf6847bca9d9ab1937fcb5a37
radzek15 <radzek15@wp.pl>
2023-07-05 03:43:23 +0100

    update adddeluser pass automation

--- README.md
+++ README.md
@@ -5,8 +5,6 @@
   ## 2. git-push
     - Script that automate the process of pushing changes to your remote repository.
 
-  ## 3. password generator
-    - Script that generates password according to chosen length.
-
-  ## 4. add del user
-    - Script to automatically add new users and delete users that are no longer needed.
+  ## 3. add del user
+    - Script to automatically add new users with generated 16 characters passwords(Mixed up,low,num,special signs), script also deletes users that are no longer needed.
+    - Password creation is easily menegeable, also "creator" needs to provide only number of accounts and username for every account, either created or deleted.
--- adddeluser.sh
+++ adddeluser.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 echo "Firstly ensure that you have super user privilages."
+
+generate_password() {
+
+    upp=$(tr -dc 'A-Z' </dev/urandom | head -c 4)
+    low=$(tr -dc 'a-z' </dev/urandom | head -c 4)
+    dig=$(tr -dc '0-9' </dev/urandom | head -c 4)
+    spe=$(tr -dc '!?!@#$%*' </dev/urandom | head -c 4)
+
+    PASSWORD=$(echo "$upp$low$dig$spe" | fold -w1 | shuf | tr -d '\n' | head -c 16)
+    echo $PASSWORD
+}
+
 read -p "How many users you want to add: " add_num
 
 for (( i = 1; i <= $add_num; i++ )); do
+  password=$(generate_password)
   read -p "Enter username #$i you want to create: " user_add
-  adduser $user_add
+  adduser $user_add --disabled-password --gecos ""
+  echo "$user_add:$password" | chpasswd
+  echo "User: $user_add, password: $password" >> new_users
 done
+
+echo "All information about created accounts you can find in file new_users in your current directory"
 
 read -p "How many users you want to delete: " del_num
 for (( i = 1; i <= $del_num; i++ )); do
